eb00c282384ffb01636a82a5287d40b02107fb65
n10o <0x0082@gmail.com>
2014-11-02 10:34:45 +0900

    Prevent duplicating

--- routes/prop.js
+++ routes/prop.js
@@ -16,63 +16,85 @@ var PropSchema = new Schema({
   smallImage: String,
   mediumImage: String,
   largeImage: String,
-  date: {type: Date, default: Date.now}
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 var Prop = mongoose.model('Prop', PropSchema);
 
-router.get('/:ownerId?', function(req, res){
+router.get('/:ownerId?', function(req, res) {
   var ownerId = req.params.ownerId;
-  Prop.find({ownerId: ownerId}, function(err, result){
+  Prop.find({
+    ownerId: ownerId
+  }, function(err, result) {
     res.json(result);
   });
 });
 
 // TODO reconsider url naming
-router.get('/item/:objectId', function(req, res){
+router.get('/item/:objectId', function(req, res) {
   var objectId = req.params.objectId;
-  Prop.find({_id: objectId}, function(err, result){
+  Prop.find({
+    _id: objectId
+  }, function(err, result) {
+    if (err) {
+      res.status(500);
+      res.send("No item found");
+    }
     res.json(result);
   });
 });
 
-router.post('/', function(req, res){
+router.post('/', function(req, res) {
   var ownerId = req.session.passport.user.id;
-  var title = req.body.Title;
-  var author = req.body.Author;
   var isbn = req.body.ISBN;
-  var publisher = req.body.Publisher;
-  var productGroup = req.body.ProductGroup;
-  var asin = req.body.ASIN;
-  var detailPageURL = req.body.DetailPageURL;
-  var smallImage = req.body.SmallImage;
-  var mediumImage = req.body.MediumImage;
-  var largeImage = req.body.LargeImage;
-  var item = new Prop({
+  Prop.find({
     ownerId: ownerId,
-    title: title,
-    author: author,
-    isbn: isbn,
-    publisher: publisher,
-    productGroup: productGroup,
-    asin: asin,
-    detailPageURL: detailPageURL,
-    smallImage: smallImage,
-    mediumImage: mediumImage,
-    largeImage: largeImage
-  });
-  item.save(function(err){
-    if (err){
-      console.log("ERROR save");
+    isbn: isbn
+  }, function(err, result) {
+    if (result.length == 0) {
+      var title = req.body.Title;
+      var author = req.body.Author;
+      var publisher = req.body.Publisher;
+      var productGroup = req.body.ProductGroup;
+      var asin = req.body.ASIN;
+      var detailPageURL = req.body.DetailPageURL;
+      var smallImage = req.body.SmallImage;
+      var mediumImage = req.body.MediumImage;
+      var largeImage = req.body.LargeImage;
+      var item = new Prop({
+        ownerId: ownerId,
+        title: title,
+        author: author,
+        isbn: isbn,
+        publisher: publisher,
+        productGroup: productGroup,
+        asin: asin,
+        detailPageURL: detailPageURL,
+        smallImage: smallImage,
+        mediumImage: mediumImage,
+        largeImage: largeImage
+      });
+      item.save(function(err) {
+        if (err) {
+          console.log("ERROR save");
+        }
+      });
     }
   });
+
   res.status(200).end();
 });
 
-router.delete('/:ownerId/:objectId', function(req, res){
+router.delete('/:ownerId/:objectId', function(req, res) {
   var ownerId = req.params.ownerId;
   var objectId = req.params.objectId;
-  Prop.findOneAndRemove({ _id: objectId, ownerId: ownerId}, function(err){
-    if(err){
+  Prop.findOneAndRemove({
+    _id: objectId,
+    ownerId: ownerId
+  }, function(err) {
+    if (err) {
       console.log("DELETE FAILED", err);
     }
   });
